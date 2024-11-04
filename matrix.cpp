6be8d3855c7b865e4ee9d779f75b08885afafecf
Keltas <inozemtseeev@gmail.com>
2024-11-04 19:08:38 +0400

    added operators + - * /

--- matrix.cpp
+++ matrix.cpp
@@ -9,8 +9,9 @@ class matrix {
 private:
 	size_t rows,cols;
 	T** array;
+	static constexpr double EPSILON = 1e-5;
 public:
-	matrix(const size_t row, const size_t col,const T& num) {
+	matrix(const size_t row, const size_t col,const T& num = 0) {
 		if (row == 0 || col == 0) {
 			throw std::invalid_argument("The matrix dimensions must be greater than zero.");
 		}
@@ -49,16 +50,71 @@ public:
 		}
 		return array[row][col];
 	}
-	size_t rows() {
-		return rows;
- 	}
-	size_t cols() {
-		return cols;
-	}
 	matrix operator + (const matrix& m) {
 		if (rows != m.rows || cols != m.cols) {
-			throw std::invalid_argument("–азмеры матриц должны совпадать дл€ сложени€");
+			throw std::invalid_argument("The matrix sizes must match for the addition");
 		}
-		matrix
+		matrix res(rows, cols);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				res.array[i][j] = array[i][j] + m.array[i][j];
+			}
+		}
+		return res;
+	}
+	matrix operator - (const matrix& m) {
+		if (rows != m.rows || cols != m.cols) {
+			throw std::invalid_argument("The matrix sizes must match for the subtraction");
+		}
+		matrix res(rows, cols);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				res.array[i][j] = array[i][j] - m.array[i][j];
+			}
+		}
+		return res;
+	}
+	matrix operator * (const matrix& m) {
+		if (cols != m.rows) {
+			throw std::invalid_argument("The number of columns of the first matrix"
+										"must match the number of rows of the second matrix");
+		}
+		matrix res(rows, m.cols);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < m.cols; ++j) {
+				for (int k = 0; k < cols; ++k) {
+					res.array[i][j] += array[i][k] * m.array[k][j];
+				}
+			}
+		}
+		return res;
+	}
+	matrix operator * (const T& num) {
+		matrix res(rows, cols);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				res.array[i][j] = array[i][j] * num;
+			}
+		}
+		return res;
+	}
+	matrix operator * (const T& num, const matrix& m) {
+		matrix res(rows, cols);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				res.array[i][j] = m.array[i][j] * num;
+			}
+		}
+		return res;
+	}
+	matrix operator / (const T& num) {
+		if (abs(num) < EPSILON) {
+			throw std::invalid_argument("Division by zero");
+		}
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				res.array[i][j] = m.array[i][j] / num;
+			}
+		}
 	}
 };
