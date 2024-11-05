d4d07836ee9187319a95489e6f9c7e3b5f670206
Keltas <inozemtseeev@gmail.com>
2024-11-05 16:39:05 +0400

    syntax errors fixed

--- matrix.cpp
+++ matrix.cpp
@@ -7,152 +7,151 @@ template <typename T>
 
 class matrix {
 private:
-	size_t rows,cols;
-	T** array;
+	size_t _rows,_cols;
+	T** _array;
 	static constexpr double EPSILON = 1e-5;
 public:
 	matrix(const size_t row, const size_t col,const T& num = 0) {
 		if (row == 0 || col == 0) {
 			throw std::invalid_argument("The matrix dimensions must be greater than zero.");
 		}
-		array = new T*[row];
+		_rows = row;
+		_cols = col;
+		_array = new T*[row];
 		for (int i = 0; i < row; ++i) {
-			array[i]= new T[col]
-				for (int j = 0; j < col; ++j) {
-					array[i][j] = num;
-				}
+			_array[i] = new T[col];
+			for (int j = 0; j < col; ++j) {
+				_array[i][j] = num;
+			}
 		}
+		cout << "matrix created" << '\n';
 	}
 	matrix(const size_t row, const size_t col, const T& min, const T& max) {
 		if (row == 0 || col == 0) {
 			throw std::invalid_argument("The matrix dimensions must be greater than zero.");
 		}
+		_rows = row;
+		_cols = col;
 		random_device rd;
 		mt19937 gen(rd());
 		uniform_real_distribution<> dis(min, max);
-		array = new T * [row];
+		_array = new T * [row];
 		for (int i = 0; i < row; ++i) {
-			array[i] = new T[col]
-				for (int j = 0; j < col; ++j) {
-					array[i][j] = dis(gen);
-				}
+			_array[i] = new T[col];
+			for (int j = 0; j < col; ++j) {
+				_array[i][j] = dis(gen);
+			}
 		}
+		cout << "matrix created" << '\n';
 	}
 	matrix(const matrix& m) {
-		rows = m.rows;
-		cols = m.cols;
-		array = new T * [rows];
-		for (int i = 0; i < rows; ++i) {
-			array[i] = new T[cols]
-				for (int j = 0; j < cols; ++j) {
-					array[i][j] = m.array[i][j];
-				}
+		_rows = m._rows;
+		_cols = m._cols;
+		_array = new T * [_rows];
+		for (int i = 0; i < _rows; ++i) {
+			_array[i] = new T[_cols];
+			for (int j = 0; j < _cols; ++j) {
+				_array[i][j] = m._array[i][j];
+			}
 		}
+		cout << "matrix created" << '\n';
 	}
 	~matrix() {
-		for (int i = 0; i < rows; ++i) {
-			delete[] array[i];
+		for (int i = 0; i < _rows; ++i) {
+			delete[] _array[i];
 		}
-		delete[] array;
+		delete[] _array;
+		cout << "matrix deleted" << '\n';
 	}
 	T& operator () (size_t row, size_t col) {
-		if (i >= rows || j >= cols) {
+		if (row >= _rows || col >= _cols) {
 			throw std::out_of_range("Индекс вне границ матрицы");
 		}
-		return array[row][col];
+		return _array[row][col];
+	}
+	const T operator () (size_t row, size_t col) const {
+		if (row >= _rows || col >= _cols) {
+			throw std::out_of_range("Индекс вне границ матрицы");
+		}
+		return _array[row][col];
 	}
 	matrix operator + (const matrix& m) {
-		if (rows != m.rows || cols != m.cols) {
+		if (_rows != m._rows || _cols != m._cols) {
 			throw std::invalid_argument("The matrix sizes must match for the addition");
 		}
-		matrix res(rows, cols);
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < cols; ++j) {
-				res.array[i][j] = array[i][j] + m.array[i][j];
+		matrix res(_rows, _cols);
+		for (int i = 0; i < _rows; ++i) {
+			for (int j = 0; j < _cols; ++j) {
+				res(i, j) = _array[i][j] + m(i,j);
 			}
 		}
 		return res;
 	}
 	matrix operator - (const matrix& m) {
-		if (rows != m.rows || cols != m.cols) {
+		if (_rows != m._rows || _cols != m._cols) {
 			throw std::invalid_argument("The matrix sizes must match for the subtraction");
 		}
-		matrix res(rows, cols);
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < cols; ++j) {
-				res.array[i][j] = array[i][j] - m.array[i][j];
+		matrix res(_rows, _cols);
+		for (int i = 0; i < _rows; ++i) {
+			for (int j = 0; j < _cols; ++j) {
+				res(i,j) = _array[i][j] - m(i, j);
 			}
 		}
 		return res;
 	}
 	matrix operator * (const matrix& m) {
-		if (cols != m.rows) {
+		if (_cols != m._rows) {
 			throw std::invalid_argument("The number of columns of the first matrix"
 										"must match the number of rows of the second matrix");
 		}
-		matrix res(rows, m.cols);
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < m.cols; ++j) {
-				for (int k = 0; k < cols; ++k) {
-					res.array[i][j] += array[i][k] * m.array[k][j];
+		matrix res(_rows, m._cols);
+		for (int i = 0; i < _rows; ++i) {
+			for (int j = 0; j < m._cols; ++j) {
+				for (int k = 0; k < _cols; ++k) {
+					res(i,j) += array[i][k] * m(k,j);
 				}
 			}
 		}
 		return res;
 	}
 	matrix operator * (const T& num) {
-		matrix res(rows, cols);
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < cols; ++j) {
-				res.array[i][j] = array[i][j] * num;
+		matrix res(_rows, _cols);
+		for (int i = 0; i < _rows; ++i) {
+			for (int j = 0; j < _cols; ++j) {
+				res(i,j) = array[i][j] * num;
 			}
 		}
 		return res;
 	}
-	matrix operator * (const T& num, const matrix& m) {
-		matrix res(rows, cols);
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < cols; ++j) {
-				res.array[i][j] = m.array[i][j] * num;
-			}
-		}
-		return res;
-	}
+
 	matrix operator / (const T& num) {
 		if (abs(num) < EPSILON) {
 			throw std::invalid_argument("Division by zero");
 		}
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < cols; ++j) {
-				res.array[i][j] = m.array[i][j] / num;
+		matrix res(_rows, _cols);
+		for (int i = 0; i < _rows; ++i) {
+			for (int j = 0; j < _cols; ++j) {
+				res(i, j) = *this(i,j) / num;
 			}
 		}
+		return res;
 	}
 	matrix operator = (const matrix& m) {
 		return matrix(m);
 	}
 	T trace() {
-		if (rows != cols) {
+		if (_rows != _cols) {
 			throw std::invalid_argument("The trace can only be calculated for a square matrix");
 		}
 		T sum = 0;
-		for (int i = 0; i < rows; ++i) {
-			sum += array[i][i]
+		for (int i = 0; i < _rows; ++i) {
+			sum += array[i][i];
 		}
-		retirn sum;
+		return sum;
 	}
-	ostream& operator << (std::ostream& os, const matrix& m)
-	{
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < cols; ++j) {
-				os << array[i][j] << ' ';
-			}
-			os << '\n';
-		}
-		return os
-	}
+
 	bool coplanar(const matrix& m1, const matrix& m2) {
-		if (rows != 3 || m1.rows != 3 || m2.rows != 3 || cols != 1 || m1.cols != 1 || m2.cols != 1) {
+		if (_rows != 3 || m1._rows != 3 || m2._rows != 3 || _cols != 1 || m1._cols != 1 || m2._cols != 1) {
 			throw std::invalid_argument("Matrices of size 3*1 are required to calculate their coplanarity");
 		}
 		double det = *this(0, 0) * (m1(1, 0) * m2(2, 0) - m1(2, 0) * m2(1, 0)) -
@@ -160,4 +159,39 @@ public:
 			*this(2, 0) * (m1(0, 0) * m2(1, 0) - m1(1, 0) * m2(0, 0));
 		return abs(det) < EPSILON;
 	}
+
+	size_t rows() const{ return _rows; }
+	size_t cols() const { return _cols; }
 };
+
+template <typename T>
+ostream& operator << (std::ostream& os, const matrix<T>& m)
+{
+	for (int i = 0; i < m.rows(); ++i) {
+		for (int j = 0; j < m.cols(); ++j) {
+			os << m(i,j) << ' ';
+		}
+		os << '\n';
+	}
+	return os;
+}
+
+template <typename T>
+matrix<T> operator * (const T& num, const matrix<T>& m) {
+	matrix res(m.rows(), m.cols());
+	for (int i = 0; i < m.rows(); ++i) {
+		for (int j = 0; j < m.cols(); ++j) {
+			res(i, j) = m(i,j) * num;
+		}
+	}
+	return res;
+}
+
+int main() {
+	matrix<int> m(3, 5, 10, 20);
+	matrix<int> m1(3, 5, 1);
+	cout << m;
+	cout << m1;
+	m1 = m;
+	cout << m1;
+}
